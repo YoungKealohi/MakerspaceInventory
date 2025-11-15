ce85b4bdcf2d090364deae20cd36d9c718174984
jmarcos3-max <jmarcos3@my.hpu.edu>
2025-11-14 21:31:58 -1000

    Change page title to 'Makerspace Inventory'

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const pool = require("../db/pool");
 
 router.get('/', (req, res) => {
-    res.render('index', { title: "Index" });
+    res.render('index', { title: "Makerspace Inventory" });
 });
 
 module.exports = router;
