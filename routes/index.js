5fe2483ec7742f3ff55d890c2c20d3e5137e742a
YoungKealohi <kyoung23@my.hpu.edu>
2025-11-18 14:53:33 -1000

    this doesn't work yet

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const pool = require("../db/pool");
 
 router.get('/', (req, res) => {
-    res.render('index', { title: "Welcome" });
+    res.render('index', { title: "Welcome", page: "index" });
 });
 
 module.exports = router;
