81cdea7b9471e70aac7110ac7be898587b1d709e
YoungKealohi <kyoung23@my.hpu.edu>
2025-11-18 15:09:21 -1000

    cahnage

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const pool = require("../db/pool");
 
 router.get('/', (req, res) => {
+    console.log("INDEX ROUTE HIT");
     res.render('index', { title: "Welcome", page: "index" });
 });
 
