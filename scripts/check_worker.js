0abebc64ed7ea0b90c7e43e0a6406c869e5d63eb
John Marcos <jmarcos3@my.hpu.edu>
2025-12-03 18:36:19 -1000

    mkae changes

---
+++ scripts/check_worker.js
@@ -0,0 +1,3 @@
+// Archived: moved to scripts/removed/check_worker.js
+// Original file content has been archived to keep the repository tidy.
+// If you need this script back, find it under scripts/removed/check_worker.js
